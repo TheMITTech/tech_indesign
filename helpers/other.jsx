421bcfa48406a68abcb33d7a5e393d47b74f22ec
Tareq El Dandachi <tareqdandachi@gmail.com>
2019-10-10 16:43:19 -0400

    Trying file permissions with random file name instead

--- helpers/other.jsx
+++ helpers/other.jsx
@@ -1,6 +1,16 @@
+TechLib.randomString = function(n) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < n; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
 TechLib.putDataToTempFile = function(data) {
 
-    var file_name = TechLib.TEMP_FILE_LOCATION+'temp.xml';
+    var file_name = TechLib.TEMP_FILE_LOCATION+TechLib.randomString(10)+'temp.xml';
     var temp = new File(file_name);
 
     temp.encoding = 'UTF-8';
